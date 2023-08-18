c05c52b26d30b07f0951cf0ca84750e6dd0abf2c
MatheusBraga71 <matheus_pivoto@gec.inatel.br>
2023-08-18 16:10:00 -0300

    Finalizando o menu interativo - Execução indefinida até selecionar opção de encerramento

--- Aula2/Desafio_Aula2.js
+++ Aula2/Desafio_Aula2.js
@@ -4,25 +4,24 @@ class Filme{
         this.ano = ano;
         this.genero = genero;
         this.duracao = duracao;
-        this.assistido = false;
+        this.assistido = 'Não';
         this.avaliacao = null;
     }
 
     exibirInfo(nome){
+        console.log('-----------------------');
         console.log(`Título: ${this.titulo}`);
         console.log(`Ano: ${this.ano}`);
         console.log(`Gênero: ${this.genero}`);
         console.log(`Duração: ${this.duracao} minutos`);
         console.log(`Já foi assistido? => ${this.assistido}`);
-        if (this.avaliacao !== null){
-            console.log(`Avaliação: ${this.avaliacao}/10`);
-        }
-        
+        console.log(`Avaliação: ${this.avaliacao}/10`);
+        console.log('-----------------------');
         console.log(''); // Puramente por estética, para os logs ficarem organizados
     }
 
     assistir(){
-        this.assistido = true;
+        this.assistido = 'Sim';
         console.log(`Filme ${this.titulo} marcado como assistido.`);
     }
 
@@ -64,28 +63,33 @@ function adicionarFilme(){
                 listaFilmes.push(filme);
                 console.log('Filme adicionado com sucesso.');
                 console.log('');
+                iniciarMenu(); 
             });
           });
         });
-    });
-    iniciarMenu(); 
+    });  
 }
 
 function marcarAssistido() {
-    listarFilmes();
-    rl.question('Digite o número do filme que deseja marcar como assistido: ', (numero) => {
-      const index = parseInt(numero) - 1;
-      if (index >= 0 && index < listaFilmes.length) {
-        listaFilmes[index].assistir();
-      } else {
-        console.log('Número de filme inválido.');
+    for (let i = 0; i < listaFilmes.length; i++) {
+        console.log(`${i+1} - ${listaFilmes[i].titulo}`);
       }
+    rl.question('Digite o número do filme que deseja marcar como assistido: ', (numero) => {
+        const index = parseInt(numero) - 1;
+        if (index >= 0 && index < listaFilmes.length) {
+        listaFilmes[index].assistir();
+        } else {
+            console.log('Número de filme inválido.');
+        }
+        iniciarMenu();
     });
-    iniciarMenu();
+    
   }
 
 function avaliarFilme() {
-    listarFilmes();
+    for (let i = 0; i < listaFilmes.length; i++) {
+        console.log(`${i+1} - ${listaFilmes[i].titulo}`);
+    }
     rl.question('Digite o número do filme que deseja avaliar: ', (numero) => {
         const index = parseInt(numero) - 1;
         if (index >= 0 && index < listaFilmes.length) {
@@ -93,28 +97,28 @@ function avaliarFilme() {
             const nota = parseInt(avaliacao);
             listaFilmes[index].avaliar(nota);
             console.log('');
+            iniciarMenu();
         });
         } else {
             console.log('Número de filme inválido.');
             console.log('');
+            iniciarMenu();
         }
+        
     });
-    iniciarMenu();
+    
 }
 
 function listarFilmes() {
     console.log('Lista de Filmes:');
     for (let i = 0; i < listaFilmes.length; i++) {
-      const filme = listaFilmes[i];
-      const assistido = filme.assistido ? '✔' : ' ';
-      const avaliacao = filme.avaliacao !== null ? `(${filme.avaliacao}/5)` : '';
-      console.log(`${i + 1}. ${assistido} ${filme.nome} (${filme.ano}) - ${filme.genero} - ${filme.duracao} ${avaliacao}`);
-      console.log('');
+      listaFilmes[i].exibirInfo();
     }
+    iniciarMenu();
 }
 
 function encerrarPrograma() {
-    console.log('Encerrando programa.');
+    console.log('Programa Finalizado!.');
     rl.close();
 }
 

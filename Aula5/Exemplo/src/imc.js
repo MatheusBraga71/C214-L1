986ce7ea4048fb1deaa6bb53fb1ce5e55e86ee9b
MatheusBraga71 <matheus_pivoto@gec.inatel.br>
2023-09-08 09:21:03 -0300

    Adicionando nova funcionalidade e testando

--- Aula5/Exemplo/src/imc.js
+++ Aula5/Exemplo/src/imc.js
@@ -1,11 +1,62 @@
-function calcularIMC (peso, altura){
-    if (altura === 0){
-        return 'NaN'; // Retorna caso a altura seja zero, não preenchida
+function calcularIMC(peso, altura) {
+    if (altura === 0) {
+      return 'NaN'; // Retorna "NaN" se a altura for igual a zero
     }
-
-    const alturaMetros = altura/100;
-    
-    return (peso / (alturaMetros * alturaMetros)).toFixed(2);
+  
+    const alturaMetros = altura / 100; // Converte altura de centímetros para metros
+    return (peso / (alturaMetros * alturaMetros)).toFixed(2); // Calcula o IMC com duas casas decimais
 }
 
-module.exports = {calcularIMC}; // Módulo responsável pela exportação da função para coleta no teste.
+// Função para cálculo da quantidade de calorias exigida pelo metabolismo da pessoa
+
+function calcularCalorias(sexo, idade, peso, altura, nivelAtividade, objetivo){
+    let TMB; // Variável para armazenar o cálculo da Taxa de Metabolismo Basal
+
+    if(sexo === 'homem'){
+        TMB = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade);
+    }
+    else if (sexo === 'mulher'){
+        TMB = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade);
+    }
+
+    let fatorAtividade; // Variável que converte o nivel de atividade para o coeficiente que irá alterar o TMB;
+
+    switch (nivelAtividade) {
+        case "sedentario":
+            fatorAtividade = 1.2;
+            break;
+        case "leve":
+            fatorAtividade = 1.375;
+            break;
+        case "moderado":
+            fatorAtividade = 1.55;
+            break;
+        case "ativo":
+            fatorAtividade = 1.725;
+            break;
+        case "muitoAtivo":
+            fatorAtividade = 1.9;
+            break;
+    }
+
+    const caloriasTotais = TMB * fatorAtividade;
+
+    let metaCalorica; // Variável que armazena a meta de calorias, para caso a pessoa queira ganhar ou perder peso
+
+    if (objetivo === "perderPeso") {
+        metaCalorica = caloriasTotais - 500; // Reduzir 500 calorias para perder peso
+    }
+    else if (objetivo === "ganharPeso") {
+        metaCalorica = caloriasTotais + 500; // Aumentar 500 calorias para ganhar peso
+    }
+    else {
+        metaCalorica = caloriasTotais; // Manter o peso
+    }
+
+    return {
+        caloriasTotais: caloriasTotais.toFixed(2),
+        metaCalorica: metaCalorica.toFixed(2)
+    };
+}
+
+module.exports = { calcularIMC, calcularCalorias }; // Exporta a função calcularIMC
--- Aula5/Exemplo/test/imc.test.js
+++ Aula5/Exemplo/test/imc.test.js
@@ -1,24 +1,60 @@
 const { expect } = require('chai');
-const { calcularIMC } = require('../src/imc');
+const { calcularIMC, calcularCalorias } = require('../src/imc');
 
 describe('Calculadora de IMC', () => {
 
-  it('Calculadora deve rodar tranquilo', () => {
-    const peso = 80; // 80 kg
-    const altura = 180; // 180 cm
-
-    const IMEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
-    const IMCCalculado = calcularIMC(peso, altura);
-
-    expect(IMCCalculado).to.equal(IMEsperado);
+    it('Calculadora deve rodar tranquilo', () => {
+      const peso = 80; // 80 kg
+      const altura = 180; // 180 cm
+  
+      const IMEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+      const IMCCalculado = calcularIMC(peso, altura);
+  
+      expect(IMCCalculado).to.equal(IMEsperado);
+    });
+  
+  
+  
+    it('altura = 0, entrada inadequada', () => {
+      const peso = 70; // 70 kg
+      const altura = 0; // 0 cm
+  
+      const IMCCalculado = calcularIMC(peso, altura);
+  
+      expect(IMCCalculado).to.equal('NaN');
+    });
   });
 
-  it('altura = 0, entrada inadequada', () => {
-    const peso = 70; // 70 kg
-    const altura = 0; // 0 cm
+// Teste da calculadora de calorias
 
-    const IMCCalculado = calcularIMC(peso, altura);
+describe('Calculadora de Calorias', () => {
+   
+    it('Cálculo da Taxa de Metabolismo Basal', () => {
+        const sexo = 'homem';
+        const idade = 21;
+        const peso = 80; // peso em kilogramas
+        const altura = 178; // altura em cm
+        const nivelAtividade = 'moderado'; // sedentario, leve, moderado, ativo, muitoAtivo
+        const objetivo = 'perderPeso'; // perderPeso ou ganharPeso
 
-    expect(IMCCalculado).to.equal('NaN');
-  });
+        if(sexo === 'homem'){
+            const TMBEsperado = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade);
+        }
+        else if (sexo === 'mulher'){
+            const TMBEsperado = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade);
+        }
+        const TMBCalculado = calcularCalorias(sexo, idade, peso, altura, nivelAtividade, objetivo)
+
+        expect()
+    });
+
+    
+    it('Cálculo das calorias Totais', () => {
+
+
+
+    });
+    
+
 });
+
